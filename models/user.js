a621c3ecc26b9c472aeb2b9c2a38206db876d5c4
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-12 17:42:21 +0530

    tokenbased

--- models/user.js
+++ models/user.js
@@ -1,70 +1,69 @@
 
-const {createHmac, randomBytes} =require("crypto");
-const { Schema, model } =require('mongoose');
-
+const { createHmac, randomBytes, generateKey } = require("crypto");
+const { Schema, model } = require('mongoose');
+const { createTokenForUser } = require("../services/authentication");
 const userSchema = new Schema({
-    fullName:{
+    fullName: {
         type: String,
         required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    salt:{
-        type:String,
+    salt: {
+        type: String,
     },
-    password:{
-        type:String,
+    password: {
+        type: String,
         required: true,
     },
-    profileImageURL:{
+    profileImageURL: {
         type: String,
         default: "/images/default.jpg",
-        
+
     },
-    role:{
+    role: {
         type: String,
-        enum:["USER", "ADMIN"],
-        default:"USER", 
+        enum: ["USER", "ADMIN"],
+        default: "USER",
     },
 },
-    {timestamps: true}
+    { timestamps: true }
 
 );
 
-userSchema.pre("save", function(next) {
+userSchema.pre("save", function (next) {
     const user = this;
-    if(!user.isModified("password")) return;
-    const salt =randomBytes(16).toString();
+    if (!user.isModified("password")) return;
+    const salt = randomBytes(16).toString();
     const hashedPassword = createHmac("sha256", salt)
-          .update(user.password)
-          .digest("hex");
+        .update(user.password)
+        .digest("hex");
     this.salt = salt;
-    this.password =hashedPassword;
+    this.password = hashedPassword;
     next();
 
-} )
+})
 
-
-
-userSchema.static("matchPassword", async function(email, password){
+userSchema.static("matchPasswordAndGenrateToken", async function (email, password) {
     const user = await this.findOne({ email });
     if (!user) throw new Error('User not found');
 
-    const salt =user.salt;
+    const salt = user.salt;
     const hashedPassword = user.password;
 
-    const userProvidedHash= createHmac("sha256", salt)
-    .update(password)
-    .digest("hex");
+    const userProvidedHash = createHmac("sha256", salt)
+        .update(password)
+        .digest("hex");
 
+    if (hashedPassword !== userProvidedHash) throw new Error("Incorrect password");
 
-    if(hashedPassword !== userProvidedHash)throw new Error("Incorrect password");
+    const token = createTokenForUser(user);
+    return token;
 
-    return user;
 });
 
-const User = model('user',  userSchema);
-module.exports =User;
+const User = model('user', userSchema);
+module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,30 @@
 const { Router } = require("express");
-const User =require('../models/user');
+const User = require('../models/user');
 
 const router = Router();
 
-router.get('/signin',  (req, res) =>{
+router.get('/signin', (req, res) => {
     return res.render('signin')
 })
 
-router.get('/signup',  (req, res) =>{
+router.get('/signup', (req, res) => {
     return res.render('signup')
 })
 
-router.post('/signin',  async (req, res) =>{
-    const { email, password} = req.body;
-    const user =  await User.matchPassword(email, password);
-    console.log('User', user);
-    return res.redirect("/");
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const token = await User.matchPasswordAndGenrateToken(email, password);
+        return res.cookie("token", token).redirect("/");
+
+    } catch (error) {
+        return res.render("signin", {
+            error: "Incorrect Email or password! "
+        })
+    }
 })
 
-router.post('/signup',  async (req, res) =>{
+router.post('/signup', async (req, res) => {
     const { fullName, email, password } = req.body;
     await User.create({
         fullName,
