0525ec39ccd9f1b04b9300c3f8764938c1ab0d72
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-12 12:21:41 +0530

    routes

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
-const { Schema, model } =require('mongoose')
+
+const {createHmac, randomBytes} =require("crypto");
+const { Schema, model } =require('mongoose');
 
 const userSchema = new Schema({
     fullName:{
@@ -33,5 +35,19 @@ const userSchema = new Schema({
 
 );
 
+userSchema.pre("save", function(next){
+    const user = this;
+    if(!user.isModified("password")) return;
+    const salt =randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256",salt)
+    .update(user.password)
+    .digest("hex");
+    this.salt = salt;
+    this.password =hashedPassword;
+    next();
+
+} )
+
 
 const User = model('user',  userSchema);
+module.exports =User;
