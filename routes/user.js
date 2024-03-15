c2550d8934a2f1270e17af14326939ce723aaa51
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-15 20:16:51 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,4 @@
 const { Router } = require("express");
-
 const User = require('../models/user');
 const router = Router();
 
@@ -8,41 +7,47 @@ router.get('/signin', (req, res) => {
     return res.render('signin')
 })
 
-
 router.get('/signup', (req, res) => {
     return res.render('signup')
 })
 
+
 router.post('/signin', async (req, res) => {
     const { email, password } = req.body;
-
     try {
+
         const token = await User.matchPasswordAndGenrateToken(email, password);
         return res.cookie("token", token).redirect("/");
+        
 
     } catch (error) {
         return res.render("signin", {
+
             error: "Incorrect Email or password!"
         });
     }
 });
 
 
+
 router.get('/logout', (req,res) =>{
     res.clearCookie("token").redirect("/");
 })
 
-router.post('/signup', async (req, res) => {
-    const { fullName, email, password } = req.body;
 
+router.post('/signup', async (req, res) => {
+
+    const { fullName, email, password } = req.body;
     await User.create({
+
         fullName,
         email,
         password,
-    });
 
+    });
     return res.redirect("/");
 });
+
 
 
 module.exports = router;
